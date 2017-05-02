462955c3c89f0a53e1f545554bcef2c1480c3994
Tijmen de Graaff <tijmen76@gmail.com>
2017-05-02 19:32:42 +0200

    add auth to edit delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
   def index
@@ -23,16 +24,16 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -51,5 +52,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
