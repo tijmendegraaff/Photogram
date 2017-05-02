60c3901ce54ec117bb819e248e06641d2d670bfb
Tijmen de Graaff <tijmen76@gmail.com>
2017-05-02 21:47:58 +0200

    add ajax delete to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,12 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "You commented the hell out of that post!"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
-      flash[:alert] = "Check the comment form, something went horribly wrong."
+      flash[:alert] = "Check the comment form, something went wrong."
       render root_path
     end
   end
@@ -17,9 +19,13 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @post.comments.find(params[:id])
 
-    @comment.destroy
-    flash[:success] = "Comment deleted :("
-    redirect_to root_path
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
 
   private
